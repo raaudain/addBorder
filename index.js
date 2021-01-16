656e5210b6fb38fff5aa0452434c96437fda6bfd
Ramon Audain <raudain123@gmail.com>
2021-01-16 01:56:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+function addBorder(picture) {
+    const charsLength = picture[0];
+    const bordered = [];
+    
+    for (let chars of picture) {
+        bordered.push("*" + chars + "*");
+    }
+    
+    bordered.unshift("*".repeat(charsLength.length + 2));
+    bordered.push("*".repeat(charsLength.length + 2));
+    
+    return bordered;
+}
